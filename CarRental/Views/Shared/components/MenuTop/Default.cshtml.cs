5986b0f0151888b12afba7ab7afb14cd874cc177
quanzz123 <nguyenquandc37@gmail.com>
2024-11-01 10:14:28 +0700

    menutop

---
+++ CarRental/Views/Shared/components/MenuTop/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRental.Views.Shared.components.MenuTop
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
