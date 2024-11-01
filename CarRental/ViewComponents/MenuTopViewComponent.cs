6461a7a5f7f7cedb557044233ce86b6e3b3c91ad
quanzz123 <nguyenquandc37@gmail.com>
2024-11-01 22:33:00 +0700

    creatê menutop

---
+++ CarRental/ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.ViewComponents
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        private readonly DbRenalCarContext _context;
+
+        public MenuTopViewComponent(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.Menus.Where(m => (bool)m.IsActive)
+                .OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
