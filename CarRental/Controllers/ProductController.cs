89fc386bfc1d779a111e560f5c9850cf0c99c36c
quanzz123 <nguyenquandc37@gmail.com>
2024-11-01 23:10:54 +0700

    tạo đường đãn tới các trang và tạo trang chi tiết sản phẩm

--- CarRental/Controllers/ProductController.cs
+++ CarRental/Controllers/ProductController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarRental.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DbRenalCarContext _context;
+        public ProductController(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+        [Route("/shop.html")]
         public IActionResult Index()
         {
             return View();
         }
+        [Route("/product/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Cars.FirstOrDefaultAsync(m => m.CarId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.productFeatured = _context.Cars.Where(i => i.Rate >= 3).OrderByDescending(i => i.Rate).ToList();
+            return View(product);
+        }
+
+
     }
 }
---
+++ CarRental/Utilities/Function.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental.Utilities
+{
+    public class Function
+    {
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
