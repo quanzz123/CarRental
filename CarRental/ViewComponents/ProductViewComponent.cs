5d548b7355aa375449fc158fc99bb7e751b1a03a
quanzz123 <nguyenquandc37@gmail.com>
2024-11-01 22:48:18 +0700

    tạo trang sản phẩm

---
+++ CarRental/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.ViewComponents
+{
+    public class ProductViewComponent :ViewComponent
+    {
+        private readonly DbRenalCarContext _context;
+
+        public ProductViewComponent(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.Cars.Include(m => m.CarType).OrderByDescending(m => m.CarId);
+
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
