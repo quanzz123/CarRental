37798a6a9f671e3f96e5185d06395e6b8777deb8
quanzz123 <nguyenquandc37@gmail.com>
2024-11-02 23:33:09 +0700

    thêm viewcomponents  cartype( not completed yet)

---
+++ CarRental/ViewComponents/CarTypesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.ViewComponents
+{
+    public class CarTypesViewComponent :ViewComponent
+    {
+        private readonly DbRenalCarContext _context;
+
+        public CarTypesViewComponent(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cartype = _context.CarTypes;
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
