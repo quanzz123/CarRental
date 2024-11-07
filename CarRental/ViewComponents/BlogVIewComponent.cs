d9ef25f181e4c35f12173ffd72b2ab04c6fa9408
quanzz123 <nguyenquandc37@gmail.com>
2024-11-07 12:14:37 +0700

    blog

---
+++ CarRental/ViewComponents/BlogVIewComponent.cs
@@ -0,0 +1,22 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+
+namespace CarRental.ViewComponents
+{
+    public class BlogVIewComponent : ViewComponent
+
+    {
+        private readonly DbRenalCarContext _context;
+        public BlogVIewComponent (DbRenalCarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var item = _context.Blogs.Where(i => (bool)i.IsActive).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(item));
+        }
+    }
+}
