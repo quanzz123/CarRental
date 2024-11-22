09423f6626fdbba362b1898f5b65da9d69520bd1
quanzz123 <nguyenquandc37@gmail.com>
2024-11-22 09:28:31 +0700

    Menu Create page for  Dashboard

---
+++ CarRental/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,88 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuController : Controller
+    {
+        private readonly DbRenalCarContext _context;
+        public MenuController(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var mnList = _context.Menus.OrderBy(m => m.MenuId).ToList();
+            return View(mnList);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var mn = _context.Menus.Find(id);
+             if (mn == null)
+            {
+                return NotFound();
+            }
+            return View(mn);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var deleMenu = _context.Menus.Find(id);
+            if (deleMenu == null)
+            {
+                return NotFound();
+            }
+            _context.Menus.Remove(deleMenu);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var mn = _context.Menus.Find(id);
+            if (mn == null)
+            {
+                return NotFound();
+            }
+            return View(mn);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Menu mn)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Menus.Update(mn);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mn);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Menu mn)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Menus.Add(mn);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mn);
+        }
+    }
+}
