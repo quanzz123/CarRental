68dbb7571b991ac1475a0e3bc9dbd222b4699994
quanzz123 <nguyenquandc37@gmail.com>
2024-11-30 01:48:36 +0700

    add to cart (update on cartcontroller)

--- CarRental/Controllers/CartController.cs
+++ CarRental/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using CarRental.Extensions;
 using CarRental.Models;
+using CarRental.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarRental.Controllers
@@ -12,7 +13,7 @@ namespace CarRental.Controllers
             _context = context;
         }
         const string CART_KEY = "MYCART";
-        public List<Car> CART => HttpContext.Session.Get<List<Car>>(CART_KEY) ?? new List<Car> ();
+        public List<CartItemsVM> CART => HttpContext.Session.Get<List<CartItemsVM>>(CART_KEY) ?? new List<CartItemsVM> ();
         public IActionResult Index()
         {
             return View(CART);
@@ -21,11 +22,42 @@ namespace CarRental.Controllers
         public IActionResult AddToCart(int id, int quantity = 1)
         {
             var cart = CART;
-            var item = cart.FirstOrDefault(p => p.CarId == id);
-            if (item == null) { 
-                
+            // lấy thông tin sản phảm từ DB
+            var product = _context.Cars.FirstOrDefault(p => p.CarId == id);
+            if (product == null) {
+
+                return NotFound();// nếu không tìm thấy sp
             }
-            return View();
+            decimal finalPrice;
+            if (product.SalePrice > 0)
+            {
+                finalPrice = product.SalePrice ?? 0;
+            } else 
+            {
+                finalPrice = product.Price ?? 0;
+            }
+            // kiểm tra xem có sản phẩm tồn tai trong giỏ hàng hay chưa
+            var CarItem = cart.FirstOrDefault(p => p.CartId == id);
+            if (CarItem == null)
+            {
+                // nếu chưa có sản phẩm thì khởi tạo sản phẩm mới
+                CarItem = new CartItemsVM
+                {
+                    CartId = product.CarId,
+                    CarName = product.CarName,
+                    Image = product.Image,
+                    Price = finalPrice,
+                    Quantity = quantity,
+                    
+                };
+                cart.Add(CarItem);
+            } else
+            {
+                // nếu đãn có thì tăng số lượng len
+                CarItem.Quantity += quantity;
+            }
+            HttpContext.Session.Set(CART_KEY, cart);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ CarRental/ViewModels/CartItemsVM.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRental.ViewModels
+{
+    public class CartItemsVM
+    {
+        public int CartId { get; set; }
+        public string CarName { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal PriceTotal => Price * Quantity;
+    }
+}
