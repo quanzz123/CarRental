03146bf6e59fbc63b33cb2d7e15063872e070d30
doanthanhthuy1811@gmail.com <doanthanhthuy1811@gmail.com>
2025-01-08 17:03:51 +0700

    newC

---
+++ CarRental/ViewComponents/NewsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.ViewComponents
+{
+    public class NewsViewComponent : ViewComponent
+    {
+        private readonly DbRenalCarContext _context;
+
+        public NewsViewComponent(DbRenalCarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.News.Where(m => (bool)m.IsActive).
+                OrderBy(m => m.NewsId).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
