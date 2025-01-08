a4eb00e0fc2566bc18ad1f6471603de7d6eba7b0
quanzz123 <nguyenquandc37@gmail.com>
2025-01-08 23:57:39 +0700

    fixbug login admin

--- CarRental/Areas/Admin/Controllers/LoginController.cs
+++ CarRental/Areas/Admin/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using CarRental.Models;
 using CarRental.Utilities;
+using CarRental.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarRental.Areas.Admin.Controllers
@@ -20,16 +21,17 @@ namespace CarRental.Areas.Admin.Controllers
         [Area("Admin")]
 
         [HttpPost]
-        public IActionResult Index(Account acc)
+        public IActionResult Index(AdminAccountVM model)
         {
-            if (acc == null) {
+            if (model == null) {
 
                 return NotFound();
             }
             //ma hoa mat khau truoc khhi kiem tra
-            string pw = Function.MD5Password(acc.Password);
+            string pw = Function.MD5Password(model.Password);
             //kiem tra email co ton tai trong DB
-            var check = _context.Accounts.Where(m => (m.Email == acc.Email) || (m.Password == pw) ).FirstOrDefault();
+            var check = _context.Accounts.Where(m => m.Email == model.Email && m.Password == pw).FirstOrDefault();
+
             if (check == null) {
                 //hin thi thong bao neu sau mk
                 Function._Message = "sai mat khau hoac emai";
